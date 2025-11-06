04fd7b4f6c6c7ac28196374591588c45252c3101
github-ci <you@example.com>
2025-11-06 12:06:05 +0800

    fix+++++++++++

--- src/DifyDatasetsBundle.php
+++ src/DifyDatasetsBundle.php
@@ -4,8 +4,26 @@ declare(strict_types=1);
 
 namespace Tourze\DifyDatasetsBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use HttpClientBundle\HttpClientBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DifyCoreBundle\DifyCoreBundle;
+use Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle;
+use Tourze\DoctrineSnowflakeBundle\DoctrineSnowflakeBundle;
+use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
 
-final class DifyDatasetsBundle extends Bundle
+final class DifyDatasetsBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            DoctrineBundle::class => ['all' => true],
+            DoctrineIndexedBundle::class => ['all' => true],
+            DoctrineSnowflakeBundle::class => ['all' => true],
+            DoctrineTimestampBundle::class => ['all' => true],
+            DifyCoreBundle::class => ['all' => true],
+            HttpClientBundle::class => ['all' => true],
+        ];
+    }
 }
